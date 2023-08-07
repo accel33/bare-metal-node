3c79a64370799cdc7c0cd86f25dca0d70b091c39
Sahil <sahil400653@gmail.com>
2023-08-07 19:07:16 +0000

    updated routes

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -4,11 +4,21 @@ const products = require('../data/products')
 const headerJson = { 'Content-Type': 'application/json' }
 
 const rutaProductos = (req, res) => {
-  if (req.url === '/products' && req.method === 'GET') {
-    res.writeHead(200, { 'Content-Type': 'application/json' })
+  if (req.url === '/products') {
+    if (req.method === 'GET') {
+      res.writeHead(200, { 'Content-Type': 'application/json' })
 
-    const products = getProducts()
-    res.end(JSON.stringify(products))
+      const products = getProducts()
+      res.end(JSON.stringify(products))
+    }
+    else if (req.method === 'POST'){
+      res.writeHead(201, headerJson)
+      res.end(JSON.stringify({'message' : 'Work in Progress..'}))
+    }
+    else{
+      res.writeHead(405, headerJson)
+      res.end(JSON.stringify({'message' : `${req.method} method is not allowed`}))
+    }
   }
   //  else if (req.url === '/productos/1' && req.method === "GET") {
   //   let id = req.url.split('/').pop()
